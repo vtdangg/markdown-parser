ed900e7388b8a9b91995f911f70c911e8e30c3a1
vtdangg <vtdang@ucsd.edu>
2022-04-21 20:56:53 -0700

    Link Test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -5,4 +5,15 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    @Test
+    //create list of links
+    //use getLinks to see if both are equal
+    public void links(){
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(content), List.of("https://something.com", "some-thing.html"));
+    }
+    
+
 }
